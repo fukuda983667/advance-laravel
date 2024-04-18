a07999e16246288887c2b37e4dc4e864e34aa8a5
fukuda <sfu983667@gmail.com>
2024-04-18 20:24:55 +0900

    0-15

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -12,4 +12,19 @@ class AuthorController extends Controller
         $authors = Author::all();
         return view('index', ['authors' => $authors]);
     }
+
+    public function add()
+    {
+        return view('add');
+    }
+
+    public function create(Request $request)
+    {
+        $form = $request->all();
+        // formタグのnameとテーブルのカラム名が一致していればcreateメソッドの引数にそのまま渡せる
+        // 一致していない場合は、一致するキー名の連想配列を作成して、$requestから値を一個ずつ格納。
+        Author::create($form);
+        // '/'にgetでアクセス。routs/web.phpに処理が回る。
+        return redirect('/');
+    }
 }
